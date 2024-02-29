51aa37d518748c55fe4caea94fab461151f390c3
Georgios Kopanidis <kopanidis@me.com>
2024-02-29 12:11:33 +0000

    Add favicon and navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import Button from 'react-bootstrap/Button';
-import React from 'react';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <Button variant="primary">Primary</Button>
+      <NavBar />
     </div>
   );
 }
