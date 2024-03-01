6dd1a401cba046e5e2dff625965083d64d7afaf2
Georgios Kopanidis <kopanidis@me.com>
2024-03-01 17:05:24 +0000

    Update axios defaults and add interceptors

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://ci-pp5-wt-drf-api-41499a8cdeac.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
